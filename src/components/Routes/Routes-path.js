d9a9410d7ac2a30563105e6c8b62b6590560822c
Adil Nisar <adil22108@gmail.com>
2022-05-24 22:58:55 +0500

    a/routes set

--- src/components/Routes/Routes-path.js
+++ src/components/Routes/Routes-path.js
@@ -10,44 +10,18 @@ import Layout from "../../Layout/Layout";
 import Dashboard from "../Doctors/dashboard/Dashboard";
 import BreadCrumBar from "../BreadCrumBar";
 import DoctorProfileSettings from "../Doctors/DoctorProfile/DoctorProfileSettings";
-<<<<<<< HEAD
-import PatientAppointments from "../Doctors/dashboard/PatientAppointments";
-import Appointment from "../Doctors/Appointments/Appointment";
-=======
-import DoctorRoutes from "./DoctorRoutes/DoctorRoutes";
 import PatientRoutes from "./PatientRoutes/PatientRoutes";
->>>>>>> a756445ba8736c1a238f9fcd19468d88d2f3e53e
+import DoctorRoutes from "./DoctorRoutes/DoctorRoutes";
 export default function RoutesPath() {
   // const loc = useLocation();
-  const user = 'patient'
+  const user = "patient";
 
   return (
     <Router>
       <Switch>
         <Layout>
-<<<<<<< HEAD
-          <Route exact path="/doctor-dashboard">
-            <Dashboard />
-          </Route>
-          <Route exact path="/home">
-            <Home_main />
-            {/* <Login/ > */}
-          </Route>
-
-          <Route path="/search_doctor">
-            <SearchDoctor />
-          </Route>
-          <Route exact path="/doctor-profile">
-            <DoctorProfileSettings />
-          </Route>
-          <Route exact path="/pending-appointments">
-            <Appointment />
-          </Route>
-=======
-          {
-            user === 'patient' ? <PatientRoutes/> : <DoctorRoutes/>
-          }
->>>>>>> a756445ba8736c1a238f9fcd19468d88d2f3e53e
+          <PatientRoutes />
+          <DoctorRoutes />
         </Layout>
       </Switch>
     </Router>
